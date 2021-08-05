7785dd91ecc3d38741c270c989b7cde8edd5011a
Erik Andrén Zachrisson <erik@zachrisson.info>
2021-08-05 20:12:56 +0200

    Output response to file

--- dummy_request.py
+++ dummy_request.py
@@ -1,9 +1,15 @@
 import requests
 from requests.auth import HTTPBasicAuth
+
   
 # Making a get request
 response = requests.get('http://192.168.0.26/snapshot.cgi',
             auth = HTTPBasicAuth('erik', 'nisse6'))
+
  
+with open('image.jpg', mode='wb') as localfile:
+	localfile.write(response.content)	
 # print request object
 print(response)
+print(response.headers)
+
